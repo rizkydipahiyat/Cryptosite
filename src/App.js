4a427982fd7f1c723bbe7eee7c2e0c3754af25b2
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2021-09-24 19:34:12 +0700

    Setup Layout Antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+	Navbar,
+	Exchanges,
+	Homepage,
+	Cryptocurrencies,
+	CryptoDetails,
+	News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
 	return (
-		<div>
-			<h1>Cryptovers</h1>
+		<div className="app">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Switch>
+							<Route path="/" exact component={Homepage} />
+							<Route path="/exchanges" component={Exchanges} />
+							<Route path="/cryptocurrencies" component={Cryptocurrencies} />
+							<Route path="/crypto/:coinId" component={CryptoDetails} />
+							<Route path="/news" component={News} />
+						</Switch>
+					</div>
+				</Layout>
+				<div className="footer">
+					<Typography.Title
+						level={5}
+						style={{ color: "white", textAlign: "center" }}
+					>
+						Cryptovers <br />
+						All rights reserverd
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
+			</div>
 		</div>
 	);
 };
